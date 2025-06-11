bf09cbea470894b230cbf089ed3d418b68ab72b3
Ahnaf Abid <ahnafabid.social@gmail.com>
2025-06-11 06:35:51 +0600

    Adding the required server

--- server/utils/sendResponse.js
+++
@@ -1,6 +0,0 @@
-export function sendResponse(res, statusCode, payload) {
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.statusCode = statusCode;
-  res.end(JSON.stringify(payload));
-}
